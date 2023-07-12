874e124d09bfe35b88b8cf464220f4cadfc99c58
Prayatna Koirala <koiralaprayatna9@gmail.com>
2023-07-12 15:55:51 +0545

    Updates

---
+++ precache-manifest.ffae07b4d21cfff3ba36503aa230b79c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55e8fc85d02c9718614e",
+    "url": "/speakersclub/static/css/main.3c31a1e4.chunk.css"
+  },
+  {
+    "revision": "55e8fc85d02c9718614e",
+    "url": "/speakersclub/static/js/main.55e8fc85.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/speakersclub/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "9cc2cf2f1eb15163f839",
+    "url": "/speakersclub/static/js/2.9cc2cf2f.chunk.js"
+  },
+  {
+    "revision": "bf8297e42b6672a9bf8f",
+    "url": "/speakersclub/static/js/runtime~main.bf8297e4.js"
+  },
+  {
+    "revision": "7795ca67051b1406f3ce524d56f0a38a",
+    "url": "/speakersclub/index.html"
+  }
+];
